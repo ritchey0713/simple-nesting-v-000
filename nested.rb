4aa4a7bbe07a94ad3dbbab89cc25c1d64bcb8e92
David Ritchey <ritchey0713@gmail.com>
2018-03-26 21:56:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -110,10 +110,10 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     
+     changed_hash = [:alan_kay][:known_for] => alans_new_info
      
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for]=alans_new_info
+     programmer_hash.merge!(changed_hash)
      
 end
 
