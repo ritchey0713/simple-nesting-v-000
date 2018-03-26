311f4da4b08813c321691e8fee45f425b78d8ad4
David Ritchey <ritchey0713@gmail.com>
2018-03-26 22:04:04 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -113,7 +113,7 @@ def changing_alan
      
      
      alans_new_info = "GUI"
-     programmer_hash.each{|item| item[:known_for] = "replaced" if item[:known_for]=="Object Orientation"
+     programmer_hash.each{|item| item[:known_for] = "replaced" if item[:known_for]=="Object Orientation"}
      
 end
 
